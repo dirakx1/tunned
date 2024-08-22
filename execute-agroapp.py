2a51ed381f2a204d8bb27c2c476597c68da66a6c
dirakx <dirakx@gmail.com>
2024-08-22 15:39:52 -0500

    Adds execute file

--- execute-agroapp.py
+++ execute-agroapp.py
@@ -19,11 +19,13 @@ print (os.getenv("POWERML__PRODUCTION__KEY"))
 lamini.api_key = os.getenv("POWERML__PRODUCTION__KEY")
 
 
-# basic model runner
-llm = Lamini(model_name='meta-llama/Meta-Llama-3-8B-Instruct')
+## Using a trainned model 
+llm = Lamini(model_name='6a2eabc6bd1bef03afd75e683d64f90f5f14ff436f62a64817d225975dd9776a')
 
-dataset_id = llm.upload_file("agroapp_docs.jsonl", input_key="question", output_key="answer")
 
-llm.train(data_or_dataset_id=dataset_id)
+def generate_response(prompt):
+    response = llm.generate(prompt, output_type={"Response":"str"})
+    return response
 
-llm.train(data_or_dataset_id='8741819033ad8888e89979529d182dc4fc517f7b30d871ab94e1f164f298074b')
+iface = gr.Interface(fn=generate_response, inputs="text", outputs="text", title="AgroApp AI")
+iface.launch(share=True)
